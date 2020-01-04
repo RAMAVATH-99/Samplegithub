311253c6a602227454b4922461f781812706548b
RAMAVATH-99 <59259641+RAMAVATH-99@users.noreply.github.com>
2020-01-04 23:49:13 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,144 +1,111 @@
-package net.simplifiedcoding.ghostapp;
+package com.tabian.firebasereaddata;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Patterns;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private EditText editTextName, editTextEmail, editTextPassword, editTextPhone;
-    private ProgressBar progressBar;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // UI references.
+    private EditText mEmail, mPassword;
+    private Button btnSignIn,btnSignOut,btnViewDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //declare buttons and edit texts in oncreate
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        btnSignIn = (Button) findViewById(R.id.email_sign_in_button);
+        btnSignOut = (Button) findViewById(R.id.email_sign_out_button);
+        btnViewDatabase = (Button) findViewById(R.id.view_items_screen);
 
-        editTextName = findViewById(R.id.edit_text_name);
-        editTextEmail = findViewById(R.id.edit_text_email);
-        editTextPassword = findViewById(R.id.edit_text_password);
-        editTextPhone = findViewById(R.id.edit_text_phone);
-        progressBar = findViewById(R.id.progressbar);
-        progressBar.setVisibility(View.GONE);
 
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.button_register).setOnClickListener(this);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if(!email.equals("") && !pass.equals("")){
+                    mAuth.signInWithEmailAndPassword(email,pass);
+                }else{
+                    toastMessage("You didn't fill in all the fields.");
+                }
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                toastMessage("Signing Out...");
+            }
+        });
+
+        btnViewDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewDatabase.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     @Override
-    protected void onStart() {
+    public void onStart() {
         super.onStart();
-
-        if (mAuth.getCurrentUser() != null) {
-            //handle the already login user
-        }
-    }
-
-    private void registerUser() {
-        final String name = editTextName.getText().toString().trim();
-        final String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-        final String phone = editTextPhone.getText().toString().trim();
-
-        if (name.isEmpty()) {
-            editTextName.setError(getString(R.string.input_error_name));
-            editTextName.requestFocus();
-            return;
-        }
-
-        if (email.isEmpty()) {
-            editTextEmail.setError(getString(R.string.input_error_email));
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextEmail.setError(getString(R.string.input_error_email_invalid));
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (password.isEmpty()) {
-            editTextPassword.setError(getString(R.string.input_error_password));
-            editTextPassword.requestFocus();
-            return;
-        }
-
-        if (password.length() < 6) {
-            editTextPassword.setError(getString(R.string.input_error_password_length));
-            editTextPassword.requestFocus();
-            return;
-        }
-
-        if (phone.isEmpty()) {
-            editTextPhone.setError(getString(R.string.input_error_phone));
-            editTextPhone.requestFocus();
-            return;
-        }
-
-        if (phone.length() != 10) {
-            editTextPhone.setError(getString(R.string.input_error_phone_invalid));
-            editTextPhone.requestFocus();
-            return;
-        }
-
-
-        progressBar.setVisibility(View.VISIBLE);
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if (task.isSuccessful()) {
-
-                            User user = new User(
-                                    name,
-                                    email,
-                                    phone
-                            );
-
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    progressBar.setVisibility(View.GONE);
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(MainActivity.this, getString(R.string.registration_success), Toast.LENGTH_LONG).show();
-                                    } else {
-                                        //display a failure message
-                                    }
-                                }
-                            });
-
-                        } else {
-                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_register:
-                registerUser();
-                break;
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
